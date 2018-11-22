04e6583976d6be6a1095bb0c92157e06e31643e7
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-22 11:42:34 +0100

    form exercise

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from 'react'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Counter from './components/Counter'
 import Form from './Form'
 
 const App = (props) => (
-  <div>
-    <Counter
-      startValue={5}
-      min={-10}
-      max={10}
-    />
-    <hr></hr>
-    <Form />
-  </div>
+  <MuiThemeProvider>
+    <div>
+      <Counter
+        startValue={5}
+        min={-10}
+        max={10}
+      />
+      <hr></hr>
+      <Form />
+    </div>
+  </MuiThemeProvider>
 
 )
 
--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import TextField from 'material-ui/TextField'
 
 class Form extends React.Component {
     state = {
-        text: 'Initial Value'
+        text: " "
     }
     render() {
         return (
             <div>
-                <h1>
-                    {this.state.text}
-                </h1>
                 <input
                     type="text"
-                    value= {this.state.text}
-                    onChange={(event) => this.setState({text: event.target.value})}
+                    value={this.state.text}
+                    onChange={(event) => this.setState({ text: event.target.value })}
                 />
+                <input
+                    value={this.state.text}
+                    readOnly={true}
+                >
+                </input>
+                <br></br>
+                <TextField
+                    id="text-field-default"
+                    defaultValue="Default Value" />
 
             </div>
         )
