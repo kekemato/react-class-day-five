0c6392df2bda6da9b2a573c14dc23ff848676307
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-22 10:30:03 +0100

    making default props for Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Counter extends React.Component {
     state = {
-        number: this.props.startValue || 0
+        number: this.props.startValue
     }
 
     incNumber = () => {
@@ -16,32 +16,46 @@ class Counter extends React.Component {
         this.setState({ number: this.state.number - 1 })
     }
 
-    render = () => (
-        <div>
-            <h1>
-                {this.state.number}
-            </h1>
-            <button
-                onClick={this.incNumber}>
-                {'+'}
-            </button>
-            <button
-                onClick={this.decNumber}>
-                {'-'}
-            </button>
-            <p>
-                {
-                    this.state.number === this.props.max ?
-                        'cannot exceed set range'
-                        :
-                        this.state.number === this.props.min ? 'cannot go below set range'
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.number}
+                </h1>
+                <button
+                    onClick={this.incNumber}>
+                    +
+                </button>
+                <button
+                    onClick={this.decNumber}>
+                    -
+                </button>
+                <p>
+                    {
+                        this.state.number  === this.props.max ?
+                            'cannot exceed set range'
                             :
-                            null
-                }
-            </p>
-        </div>
-    )
+                            this.state.number === this.props.min ? 'cannot go below set range'
+                                :
+                                null
+                    }
+                </p>
+            </div>
+        )
+    }
 
+}
+
+Counter.defaultProps = {
+    startValue: 0,
+    min: -5,
+    max: 5
+}
+
+Counter.propTypes = {
+    startValue: Number,
+    min: Number,
+    max: Number
 }
 
 export default Counter
